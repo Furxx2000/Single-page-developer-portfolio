0e392c0f5f703af682044d63d0c3a286b630ac74
furxx2000 <furxx2000@gmail.com>
2023-02-02 21:39:51 +0800

    Install tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['"Space Grotesk"', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
