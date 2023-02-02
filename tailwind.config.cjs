9dc40708346dfd7b27b16f2ddd54c0d87879ed2d
furxx2000 <furxx2000@gmail.com>
2023-02-02 23:24:45 +0800

    Set design color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'deep-black': '#151515',
+      'light-black': '#242424',
+      'primary-green': '#4EE1A0',
+      gray: '#D9D9D9',
+      white: '#FFFFFF',
+    },
     fontFamily: {
       sans: ['"Space Grotesk"', 'sans-serif'],
     },
