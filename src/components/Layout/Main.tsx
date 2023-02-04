f0cb5b14ae76636a70846bb19269d2f5e7640642
furxx2000 <furxx2000@gmail.com>
2023-02-04 23:54:03 +0800

    Create component for contents

---
+++ src/components/Layout/Main.tsx
@@ -0,0 +1,13 @@
+import HeroSection from '../Content/HeroSection/HeroSection';
+import ProjectsSection from '../Content/ProjectsSection/ProjectsSection';
+import SkillSection from '../Content/SkillSection/SkillSection';
+
+export default function Main() {
+  return (
+    <main>
+      <HeroSection />
+      <SkillSection />
+      <ProjectsSection />
+    </main>
+  );
+}
