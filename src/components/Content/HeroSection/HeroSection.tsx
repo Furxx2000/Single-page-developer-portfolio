92d691251cf94e751f694aff06bdb9d7b81167f3
furxx2000 <furxx2000@gmail.com>
2023-02-04 23:52:50 +0800

    Finish hero section

---
+++ src/components/Content/HeroSection/HeroSection.tsx
@@ -0,0 +1,29 @@
+import CircleImage from './CircleImage';
+import HeroImage from './HeroImage';
+import RingsImage from './RingsImage';
+import BaseButton from '../../UI/BaseButton';
+import DeveloperName from './DeveloperName';
+import DeveloperDes from './DeveloperDes';
+
+export default function HeroSection() {
+  const developerProfile = {
+    name: 'Danny Wen',
+    description:
+      "Based in Taiwan, I'm a front-end developer passionate about building accessible web apps that users love.",
+  };
+
+  return (
+    <section className='grid gap-y-10 mb-20'>
+      <div className='relative flex justify-center overflow-x-hidden'>
+        <HeroImage />
+        <RingsImage />
+        <CircleImage />
+      </div>
+      <div className='grid gap-y-6 justify-items-center'>
+        <DeveloperName name={developerProfile.name} />
+        <DeveloperDes description={developerProfile.description} />
+        <BaseButton text='contact me' />
+      </div>
+    </section>
+  );
+}
