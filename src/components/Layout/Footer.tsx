78f0495d9034ad7bcb71be1737674e1c26d82566
furxx2000 <furxx2000@gmail.com>
2023-02-06 23:45:45 +0800

    Add footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+import Brand from '../UI/Brand';
+
+export default function Footer() {
+  return (
+    <footer className='bg-light-black pt-[39px] pb-[60px] px-[100px]'>
+      <Brand />
+    </footer>
+  );
+}
