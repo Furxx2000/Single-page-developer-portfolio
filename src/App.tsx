7629b21894cfa7bea4683534f482e500458f472b
furxx2000 <furxx2000@gmail.com>
2023-02-06 23:43:50 +0800

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Layout/Header';
 import Main from './components/Layout/Main';
+import Footer from './components/Layout/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
