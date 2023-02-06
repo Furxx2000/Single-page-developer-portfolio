3ffe7f859ca4e324e142db4536cfa562fcffc87f
furxx2000 <furxx2000@gmail.com>
2023-02-06 23:46:44 +0800

    Import Contact section and add overflow to prevent ring image broke UI

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -1,13 +1,15 @@
 import HeroSection from '../Content/HeroSection/HeroSection';
 import ProjectsSection from '../Content/ProjectsSection/ProjectsSection';
 import SkillSection from '../Content/SkillSection/SkillSection';
+import ContactSection from '../Content/ContactSection/ContactSection';
 
 export default function Main() {
   return (
-    <main>
+    <main className='overflow-x-hidden'>
       <HeroSection />
       <SkillSection />
       <ProjectsSection />
+      <ContactSection />
     </main>
   );
 }
