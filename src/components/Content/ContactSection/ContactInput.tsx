772997f94d37a0d09a5e9079723c57d40db22dca
furxx2000 <furxx2000@gmail.com>
2023-02-08 01:17:14 +0800

    take props for state management

--- src/components/Content/ContactSection/ContactInput.tsx
+++ src/components/Content/ContactSection/ContactInput.tsx
@@ -1,11 +1,20 @@
+import { BiErrorCircle } from 'react-icons/bi';
+
 interface Props {
   text: string;
   type: string;
+  error: boolean;
+  handleInputValue: (value: string) => void;
 }
 
-export default function ContactInput({ text, type }: Props) {
+export default function ContactInput({
+  text,
+  type,
+  error,
+  handleInputValue,
+}: Props) {
   return (
-    <div className='w-full'>
+    <div className='flex w-full'>
       <label htmlFor={text} className='hidden'>
         {text}
       </label>
@@ -15,9 +24,11 @@ export default function ContactInput({ text, type }: Props) {
         name={text}
         maxLength={32}
         placeholder={text}
-        className='font-medium px-6 pb-4 border-b border-white leading-[26px] tracking-[-0.22px] focus:border-primary-green transition placeholder:text-gray placeholder:opacity-50 placeholder:uppercase'
+        onChange={(e) => handleInputValue(e.target.value)}
+        className={`font-medium px-6 pb-4 border-b border-white leading-[26px] tracking-[-0.22px] focus:border-primary-green ${
+          error ? 'border-error-red' : ''
+        } transition placeholder:text-gray placeholder:opacity-50 placeholder:uppercase`}
         autoComplete='off'
-        required
       />
     </div>
   );
