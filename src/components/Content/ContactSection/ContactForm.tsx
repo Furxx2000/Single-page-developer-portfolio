51b237006f76694118a09228212e98ab85d63482
furxx2000 <furxx2000@gmail.com>
2023-02-08 01:16:54 +0800

    useState to store input value

--- src/components/Content/ContactSection/ContactForm.tsx
+++ src/components/Content/ContactSection/ContactForm.tsx
@@ -1,11 +1,55 @@
 import ContactInput from './ContactInput';
 import ContactMessage from './ContactMessage';
 import BaseButton from '../../UI/BaseButton';
+import { useState } from 'react';
 
 export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  function validation() {
+    let isValid = true;
+
+    if (name === '') {
+      isValid = false;
+      setNameError(true);
+      return isValid;
+    }
+
+    if (!email.includes('@')) {
+      isValid = false;
+      setEmailError(true);
+      return isValid;
+    }
+
+    return isValid;
+  }
+
+  function handleGetName(name: string) {
+    setName(name);
+  }
+
+  function handleGetEmail(email: string) {
+    setEmail(email);
+  }
+
+  function handleGetMessage(message: string) {
+    setMessage(message);
+  }
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log('Send message!');
+
+    const isValid = validation();
+
+    if (isValid) {
+      console.log('Submit success!');
+    } else {
+      console.log('Submit fail!');
+    }
   }
 
   return (
@@ -13,9 +57,19 @@ export default function ContactForm() {
       className='grid gap-y-8 justify-items-end mb-[27px]'
       onSubmit={(e) => handleSubmit(e)}
     >
-      <ContactInput text='name' type='text' />
-      <ContactInput text='email' type='email' />
-      <ContactMessage text='message' />
+      <ContactInput
+        text='name'
+        type='text'
+        error={nameError}
+        handleInputValue={handleGetName}
+      />
+      <ContactInput
+        text='email'
+        type='email'
+        error={emailError}
+        handleInputValue={handleGetEmail}
+      />
+      <ContactMessage text='message' handleMessageValue={handleGetMessage} />
       <BaseButton text='send message' submit />
     </form>
   );
