d00bec82164d5800894fcbce8ed48f8bc5c7c948
furxx2000 <furxx2000@gmail.com>
2023-02-08 01:17:28 +0800

    Add props for state management

--- src/components/Content/ContactSection/ContactMessage.tsx
+++ src/components/Content/ContactSection/ContactMessage.tsx
@@ -1,8 +1,9 @@
 interface Props {
   text: string;
+  handleMessageValue: (value: string) => void;
 }
 
-export default function ContactMessage({ text }: Props) {
+export default function ContactMessage({ text, handleMessageValue }: Props) {
   return (
     <div className='w-full'>
       <label htmlFor={text} className='hidden'>
@@ -14,7 +15,7 @@ export default function ContactMessage({ text }: Props) {
         rows={3}
         placeholder={text}
         className='font-medium px-6 pb-4 border-b border-white leading-[26px] tracking-[-0.22px] focus:border-primary-green transition placeholder:text-gray placeholder:opacity-50 placeholder:uppercase'
-        required
+        onChange={(e) => handleMessageValue(e.target.value)}
       ></textarea>
     </div>
   );
