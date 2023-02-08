b8ec8ed01844f7ed3651ae058aeac834c704bce5
furxx2000 <furxx2000@gmail.com>
2023-02-08 23:51:17 +0800

    Add focus handler and error icon

--- src/components/Content/ContactSection/ContactInput.tsx
+++ src/components/Content/ContactSection/ContactInput.tsx
@@ -1,10 +1,11 @@
-import { BiErrorCircle } from 'react-icons/bi';
+import ErrorIcon from '../../../assets/Error-icon.svg';
 
 interface Props {
   text: string;
   type: string;
   error: boolean;
   handleInputValue: (value: string) => void;
+  handleFocus: () => void;
 }
 
 export default function ContactInput({
@@ -12,9 +13,10 @@ export default function ContactInput({
   type,
   error,
   handleInputValue,
+  handleFocus,
 }: Props) {
   return (
-    <div className='flex w-full'>
+    <div className='relative grid gap-y-1 w-full'>
       <label htmlFor={text} className='hidden'>
         {text}
       </label>
@@ -24,12 +26,21 @@ export default function ContactInput({
         name={text}
         maxLength={32}
         placeholder={text}
+        onFocus={() => handleFocus()}
         onChange={(e) => handleInputValue(e.target.value)}
         className={`font-medium px-6 pb-4 border-b border-white leading-[26px] tracking-[-0.22px] focus:border-primary-green ${
           error ? 'border-error-red' : ''
         } transition placeholder:text-gray placeholder:opacity-50 placeholder:uppercase`}
         autoComplete='off'
       />
+      {error && (
+        <>
+          <img className='absolute right-0' src={ErrorIcon} alt='' />
+          <p className='text-error-red text-right text-xs tracking-[-0.17px]'>
+            Sorry, invalid format here
+          </p>
+        </>
+      )}
     </div>
   );
 }
