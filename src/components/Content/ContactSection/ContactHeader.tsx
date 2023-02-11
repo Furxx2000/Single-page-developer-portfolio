7d9a646927bb06194e97d9b03ed7218412daeee8
furxx2000 <furxx2000@gmail.com>
2023-02-11 13:51:57 +0800

    Add tablet style for font size

--- src/components/Content/ContactSection/ContactHeader.tsx
+++ src/components/Content/ContactSection/ContactHeader.tsx
@@ -2,7 +2,7 @@ export default function ContactHeader() {
   return (
     <div className='grid text-center gap-y-5 '>
       <h2>Contact</h2>
-      <p className='font-medium text-gray leading-[26px]'>
+      <p className='font-medium md:text-lg text-gray leading-[26px]'>
         I would love to hear about your project and how I could help. Please
         fill in the form, and I'll get back to you as soon as possible.
       </p>
