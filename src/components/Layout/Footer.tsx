565eab1a543eb3c3c3c1061eaf6a8afa1ea5c082
furxx2000 <furxx2000@gmail.com>
2023-02-13 00:44:09 +0800

    Add desktop style for layout

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Divider from '../UI/Divider';
 
 export default function Footer() {
   return (
-    <footer className='grid gap-y-9 bg-light-black pb-[60px] md:pb-10 px-4 md:px-8'>
+    <footer className='grid gap-y-9 bg-light-black pb-[60px] px-4 md:px-8 md:pb-10 xl:px-[165px] xl:gap-y-12 xl:pb-[92px]'>
       <Divider />
       <Brand />
     </footer>
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,7 +2,7 @@ import Brand from '../UI/Brand';
 
 export default function Header() {
   return (
-    <header className='absolute top-0 w-full pt-6 px-8 z-50'>
+    <header className='absolute top-0 w-full pt-6 px-8 z-50 xl:pt-10 xl:pl-[165px] xl:pr-[195px]'>
       <Brand />
     </header>
   );
