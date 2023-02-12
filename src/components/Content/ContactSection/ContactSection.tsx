abda99923adab30f17a1205c5bacefb0f54d05e4
furxx2000 <furxx2000@gmail.com>
2023-02-13 01:01:10 +0800

    Add id

--- src/components/Content/ContactSection/ContactSection.tsx
+++ src/components/Content/ContactSection/ContactSection.tsx
@@ -4,7 +4,10 @@ import RingsImage from '../../Content/HeroSection/RingsImage';
 
 export default function ContactSection() {
   return (
-    <section className='relative grid gap-y-[50px] px-4 pt-[60px] bg-light-black md:px-[165px] xl:pt-[84px] xl:grid-cols-2'>
+    <section
+      id='contact-section'
+      className='relative grid gap-y-[50px] px-4 pt-[60px] bg-light-black md:px-[165px] xl:pt-[84px] xl:grid-cols-2'
+    >
       <ContactHeader />
       <ContactForm />
       <RingsImage position='left-[-92%] bottom-[15.3%] md:left-[-47.5%] md:bottom-[5%] xl:left-[-14%] xl:bottom-[11%]' />
