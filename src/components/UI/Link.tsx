e03e7a8252ef0d2685526a1d2cc3f5413212852d
furxx2000 <furxx2000@gmail.com>
2023-02-13 00:44:24 +0800

    Remove title attribute

--- src/components/UI/Link.tsx
+++ src/components/UI/Link.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function Link({ site, link, icon }: Props) {
   return (
     <li className='hover:scale-125 hover:text-primary-green transition'>
-      <a href={link} target='_blank' title={site}>
+      <a href={link} target='_blank'>
         {icon}
         <span className='sr-only'>{site}</span>
       </a>
