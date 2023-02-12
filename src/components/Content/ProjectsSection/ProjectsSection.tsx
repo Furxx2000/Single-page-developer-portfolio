823eba2494399b5690c96ebd84b216736fb40ac1
furxx2000 <furxx2000@gmail.com>
2023-02-13 00:43:34 +0800

    Add desktop style for project section

--- src/components/Content/ProjectsSection/ProjectsSection.tsx
+++ src/components/Content/ProjectsSection/ProjectsSection.tsx
@@ -42,10 +42,10 @@ export default function ProjectsSection() {
   }, []);
 
   return (
-    <section className='relative grid gap-y-10 px-4 md:px-8 pb-20 md:pb-[100px]'>
+    <section className='relative grid gap-y-10 px-4 pb-20 md:gap-y-[60px] md:px-8 md:pb-[100px] xl:px-[165px] xl:gap-y-20'>
       <Divider classes='md:hidden' />
       <ProjectsHeader />
-      <div className='grid md:grid-cols-2 gap-y-10 md:gap-x-[22px]'>
+      <div className='prjs grid gap-y-10 md:grid-cols-2 md:gap-x-[30px] xl:gap-y-[70px]'>
         {projects.map(
           ({ projectName, image, skills, projectLink, codeLink }) => (
             <Project
@@ -59,7 +59,7 @@ export default function ProjectsSection() {
           )
         )}
       </div>
-      <RingImage position='top-[-63px] md:top-[-118px] right-[-92%] md:right-[-44%]' />
+      <RingImage position='top-[-63px] right-[-92%] md:top-[-118px] md:right-[-44%] xl:right-[-16.5%] xl:top-[-145px]' />
     </section>
   );
 }
