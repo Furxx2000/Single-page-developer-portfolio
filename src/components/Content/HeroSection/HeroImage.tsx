0dfe1490e2da0e7d29d9f0e6e6eb2acc0471b2c9
furxx2000 <furxx2000@gmail.com>
2023-03-12 22:01:08 +0800

    Add css style for img

--- src/components/Content/HeroSection/HeroImage.tsx
+++ src/components/Content/HeroSection/HeroImage.tsx
@@ -1,13 +1,17 @@
-import mobileImage from '../../../assets/image-profile-mobile.webp?url';
-import tabletImage from '../../../assets/image-profile-tablet.webp?url';
-import desktopImage from '../../../assets/image-profile-desktop.webp?url';
+import mobileImage from '../../../assets/mobile_image.jpg?url';
+import tabletImage from '../../../assets/tablet_image.jpg?url';
+import desktopImage from '../../../assets/desktop_image.jpg?url';
 
 export default function HeroImage() {
   return (
-    <picture className='block w-[174px] md:w-[322px] xl:w-[445px]'>
+    <picture className='relative z-[-15] block w-[174px] md:w-[322px]  xl:w-[445px] '>
       <source srcSet={desktopImage} media='(min-width: 1280px)' />
       <source srcSet={tabletImage} media='(min-width: 768px)' />
-      <img src={mobileImage} alt='Danny Wen' />
+      <img
+        className='h-[383px] md:h-[600px] xl:h-[720px]'
+        src={mobileImage}
+        alt='Danny Wen'
+      />
     </picture>
   );
 }
