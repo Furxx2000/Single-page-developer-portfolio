7a78ed1dbf2385df43e6e3035cc5a7384be8f049
Danny Wen <danny_wen@trendmicro.com>
2024-03-12 22:53:54 +0800

    Update interface for skills

--- src/components/Content/SkillSection/SkillSection.tsx
+++ src/components/Content/SkillSection/SkillSection.tsx
@@ -2,6 +2,11 @@ import Divider from '../../UI/Divider';
 import Skill from './Skill';
 import Author from '../../../data/data';
 
+interface Props {
+  skill: string;
+  experience?: number;
+}
+
 export default function SkillSection() {
   const skills = Author.skills;
 
@@ -9,7 +14,7 @@ export default function SkillSection() {
     <section className='grid px-4 md:px-8 xl:px-[165px]'>
       <Divider />
       <ul className='grid gap-y-6 md:gap-y-14 py-10 md:py-[52px] xl:py-[72px] md:grid-cols-2 xl:grid-cols-3'>
-        {skills.map(({ skill, experience }) => (
+        {skills.map(({ skill, experience }: Props) => (
           <Skill key={skill} skill={skill} experience={experience} />
         ))}
       </ul>
